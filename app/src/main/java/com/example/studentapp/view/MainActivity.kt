30a1d37c34de46d27a99ffe23ec4182491cd653f
Yosiahans <s160421126@student.ubaya.ac.id>
2024-03-28 15:20:02 +0700

    W6.3

--- app/src/main/java/com/example/studentapp/view/MainActivity.kt
+++ app/src/main/java/com/example/studentapp/view/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.studentapp.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.studentapp.R
+import com.example.studentapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-//    private lateint var binding:ActivityMainBinding
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
